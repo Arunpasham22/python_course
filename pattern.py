66f6431af40f0c93f96aeb2e13093e7e80a4381a
arunpasham22 <arunpasham4@gmail.com>
2025-10-28 12:01:42 +0530

    adding List programs

---
+++ pattern.py
@@ -0,0 +1,10 @@
+rows = 5
+for i in range(1, rows+1):      
+    for j in range(1, i+1):      
+        print("*", end="")      
+    print()                   
+n=6
+for i in range(1,n+1):
+    for j in range(1,n-i):
+        print('*',end='')
+    print()
